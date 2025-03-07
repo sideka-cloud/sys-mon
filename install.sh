9a2c560b89a880cb6360b353b20c448b27a4239c
sideka-cloud <89623723+sideka-cloud@users.noreply.github.com>
2025-03-07 21:01:41 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add permission execute
+chmod +x system_monitor.sh
+chmod +x read.sh
+
+# Copy file service
+cp -r system_monitor.service /etc/systemd/system/
+
+# Reload systemd
+systemctl daemon-reload
+
+# Enable service system_monitor
+systemctl enable system_monitor
+systemctl start system_monitor
