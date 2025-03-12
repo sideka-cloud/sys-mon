d17cb3576581c7bfd1c3c7fd0ecfc5465a996df1
sideka-cloud <89623723+sideka-cloud@users.noreply.github.com>
2025-03-12 19:21:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+echo "=== Install sysstat and iotop ==="
+yum install sysstat iotop -y
+echo " "
+check_package() {
+    PACKAGE=$1
+    if rpm -q "$PACKAGE" &>/dev/null; then
+        echo "$PACKAGE is installed"
+    else
+        echo "$PACKAGE is NOT installed"
+    fi
+}
+echo "=== Check if sysstat and iotop were installed ==="
+check_package "sysstat"
+check_package "iotop"
+echo " "
+echo "=== Install system_monitor ==="
+
 # Add permission execute
 chmod +x system_monitor.sh
 chmod +x read.sh
@@ -15,11 +32,11 @@ systemctl enable system_monitor
 systemctl start system_monitor
 
 echo " "
-echo "system_monitor was installed." 
+echo "=== System_monitor was installed ===" 
 echo " "
-echo "Check status service :"
+echo "=== Check status service ==="
 echo "systemctl status system_monitor"
 echo " "
-echo "Check logs files, example :"
+echo "=== Check logs files, example: ==="
 echo "bash read.sh "2025-03-07 22:00:00" "2025-03-07 22:10:00""
 echo " "
